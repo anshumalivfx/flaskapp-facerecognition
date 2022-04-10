d3a1d4e1ccc42807cffb23c80fe8f15042398030
anshumalivfx <anshumali.karna99@gmail.com>
2022-04-10 07:23:35 +0530

    Added New

--- app.py
+++ app.py
@@ -1,24 +1,49 @@
+from unicodedata import name
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, Response
+from werkzeug.utils import secure_filename
 from facerecognition import FaceRecognition
+import os
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/upload/'
 facerecognition = FaceRecognition()
+app.secret_key = 'godisbitch'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16*1024*1024
 
-def gen(camera):
-    while True:
-        #get camera frame
-        frame = camera.start()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
-@app.route('/')
+def allowed_filename(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/home')
 def index():
     return render_template('index.html')
+@app.route('/')
+def default():
+    return redirect('/home')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(FaceRecognition()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash("No File Found")
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == "":
+        flash("No Image Selected for uploading")
+        return redirect(request.url)
+    if file and allowed_filename(file.filename):
+        filename = secure_filename(file.filename)
+        print(filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        attendance = facerecognition.start(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template('index.html', attendance=attendance)
+    else:
+        flash('not Valid Image File')
+        return redirect(request.url)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- facerecognition.py
+++ facerecognition.py
@@ -44,37 +44,40 @@ class FaceRecognition:
     
     
     
-    def start(self):
+    def start(self, img):
         encodeListKnown = self.findEncodings()
         print('Encoding Complete')
-        cap = cv2.VideoCapture(0)
-        while True:
-            success, img = cap.read()
-            imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-            imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-            facesCurFrame = face_recognition.face_locations(imgS)
-            encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
-            for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-                matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-                faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-                # print(faceDis)
-                matchIndex = np.argmin(faceDis)
-                if matches[matchIndex]:
-                    name = self.classNames[matchIndex].upper()
-                    y1, x2, y2, x1 = faceLoc
-                    y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                    cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (255, 0, 0), cv2.FILLED)
-                    cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-                    nameFile = open('Attendance.csv', 'r+')
-                    nameList = nameFile.readlines()
-                    for line in nameList:
-                        entry = line.split(',')
-                        if name in entry:
-                            print('already marked')
-                            break
-                    else:
-                        self.markAttendance(name)
+        attendance = ""
+    
+        img = cv2.imread(img)
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            # print(faceDis)
+            matchIndex = np.argmin(faceDis)
+            if matches[matchIndex]:
+                name = self.classNames[matchIndex].upper()
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (255, 0, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                nameFile = open('Attendance.csv', 'r+')
+                nameList = nameFile.readlines()
+                for line in nameList:
+                    entry = line.split(',')
+                    if name in entry:
+                        attendance = "Already Marked"
+                else:
+                   
+                    self.markAttendance(name)
+                    attendance = "Marked"
             
-                ret, jpeg = cv2.imencode('.jpg', img)
-                return jpeg.tobytes()
+            else:
+                name = "Unknown"
+                attendance = "Not Marked"
+            return f'{name} {attendance}'
